6a12db2b6fdcbc551a694333e9582a0aa95ec0c2
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-01-31 17:51:28 +0800

    merge server.jar download to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 JAVA_VERSION=21
+SERVER_FILE_URL="https://piston-data.mojang.com/v1/objects/4707d00eb834b446575d89a61a11b5d548d8c001/server.jar"
 
 # Install latest version of Java
 sudo apt update && sudo apt install openjdk-$JAVA_VERSION-jre
 echo "Java $JAVA_VERSION installed."
+
+# Download "server.jar" from official Minecraft
+
+wget -O server.jar "$SERVER_FILE_URL"
+echo "Minecraft server installed."
