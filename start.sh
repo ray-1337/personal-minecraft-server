7a094699f31a4a83f0e6f7bb65665e4ad973801d
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-01-31 18:11:13 +0800

    [draft] add flags to optimize Minecraft server (from flags.sh)

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 MEMORY="4G"
+JAVA_ARG="--add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20"
 
-java -Xmx$MEMORY -Xms$MEMORY -jar server.jar nogui
+java -Xmx$MEMORY -Xms$MEMORY $JAVA_ARG -jar server.jar nogui
