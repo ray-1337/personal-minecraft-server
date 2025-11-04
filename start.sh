ae5620f01a86c11d87930136cfd922e9d8de07c4
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-11-04 12:34:43 +0800

    Replace missing "MAX_MEMORY" arg with "INITIAL_MEMORY"

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 INITIAL_MEMORY="3G"
 JAVA_ARG="-Xmn512m -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UseCompressedOops -XX:-UsePerfData -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=4 -XX:ConcGCThreads=2 -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=50 -XX:G1HeapRegionSize=1 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=8"
 
-java -Xmx$MAX_MEMORY -Xms$INITIAL_MEMORY $JAVA_ARG -jar server.jar nogui
+java -Xmx$INITIAL_MEMORY -Xms$INITIAL_MEMORY $JAVA_ARG -jar server.jar nogui
